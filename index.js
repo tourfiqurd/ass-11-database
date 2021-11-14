86e8e5c8f1b472117a614aebc74435124d1567fb
tourfiqurd <anquire05@gmail.com>
2021-11-14 22:31:38 +0600

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require("mongodb").ObjectId
 const cors = require("cors")
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 //asi-11
 //OPwwzvdaGg4C1SqE
